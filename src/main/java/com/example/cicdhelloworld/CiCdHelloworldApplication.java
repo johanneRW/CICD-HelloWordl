38a833d3d24346068e764818b9970fd3d9f9a563
johanne <johanne_riis@hotmail.com>
2022-08-26 08:58:20 +0200

    First commit

---
+++ src/main/java/com/example/cicdhelloworld/CiCdHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdHelloworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdhelloworld/CiCdHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
